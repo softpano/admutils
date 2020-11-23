36b27e104d91141411aa9394955b5ecdd7da0444
softpano <47441890+softpano@users.noreply.github.com>
2020-11-23 09:11:57 -0500

    Add files via upload

--- dir_favorites_shell_functions.sh
+++ dir_favorites_shell_functions.sh
@@ -1,26 +1,71 @@
 #!/usr/bin/bash
-# Last modified Oct 19, 2020
+# Intelligent cd. 
+#
+# Copyright Nikolai Bezroukov 2007-2020
+# Licensed under the original BSD license
+#
+# It is minimal not to screw up the things.  
+# Displays dirs stack and allow tro select an entry; or expand supplied string into a path to directory according to rules descibed below
+#
+# 1. If no parameters given displays dirs stack and allow to to provide a reply string which can be a number or string 
+#    a. If the reply is the bumber selects the directory from dirs stack with this number counting from top 
+#       For countring from the bottom the number should be prefixed by minus
+#    b. If the reply is string that does not contain / invokes fcd function and expland it into path to the directory according to rules (2) and (3) below
+#    c. If the reply contains / execute rregular cd command with this parameter. 
+#    
+# 2. if one parameter is given, it is split into letters and asterisks and slashes put in between 
+#    For example, 
+#        go ulb 
+#    is equivalent to 
+#        cd /u*/l*/b*
+#
+# 3. If multiple parameters are given they are concateneted with */
+#    For example 
+#       go use loc bi 
+#    is equvalent to 
+#       cd /use*/loc*/bi*
+#
+# NOTE: Functionality of dir_favorites.pl exlained elsewhere. Type dir_favirites.pl -h for help. 
+#
+#-------------------------------------------------------------------------------------------  
 # Ver      Date        Who        Modification
 # ====  ==========  ========  ==============================================================
-# 1.00  2020/11/19  Bezroun   .config is now used 
-#---------------------------------------------------------------
-# Intelligent go. It is minimal not to screw up the things. Normally you can use fav. 
+# 0.10  2007/01/02  Bezroun   Initial implementation 
+# 0.20  2008/05/16  Bezroun   fcd function added, which in a non-obious way to imitate ncd. 
+# 0.30  2008/06/01  Bezroun   dir_favorites is now integrated
+# 0.40  2009/09/10  Bezroun   generation now includes aliases cd-- cd-- and cd-3
+# 1.00  2020/11/19  Bezroun   .config is now used for static favorites and temp files
+# 1.10  2020/11/23  Bezroun   Polishing before publishing 
+  
 #---------------------------------------------------------------
 function go {
+   if (( $# > 0 )); then 
+      fcd $@
+      return
+   fi   
+    
    perl $HOME/bin/dir_favorites.pl -m > $HOME/.config/dir_current.tmp 
    . $HOME/.config/dir_current.tmp
    dirs -v
-   echo -n 'Select favorite (postive counting from top, negative from bottom): '
+   echo -n 'Select favorite (postive counting from top, negative from bottom) or copy the path: '
    read target
-   if (( target < 0 )); then
-      echo $target
-      mydir=`dirs -l $target`
-      cd $mydir
-   elif (( target >=0 )); then
-      echo +$target
-      mydir=`dirs -l +$target`
-      cd $mydir
-   fi
+   echo $target
+   if [[ $target =~ '/' ]]; then 
+      #echo absolute
+      cd $target
+   else
+      #echo numeric 
+      if (( target < 0 )); then
+         echo $target
+         mydir=`dirs -l $target`
+         cd $mydir
+      elif (( target > 0 )); then
+         echo +$target
+         mydir=`dirs -l +$target`
+         cd $mydir
+      fi   
+   fi   
+   
 }
 #
 # Writes current dir into DIRHISTORY
@@ -64,4 +109,35 @@ function my_prompt
       #echo Updating favorites each time we have  number of sec divisible by 5
       perl $HOME/bin/dir_favorites.pl -m > $HOME/.config/dir_current.tmp; . $HOME/.config/dir_current.tmp      
    fi
+}
+#----------------------------- ncd imitation
+# Provide short one letter abberiation and long multiletter abbreviation separated by space
+function fcd 
+{
+local STRING=$1
+local NEWDIR=''
+   if (( $# == 1 )); then
+      local i=0
+      LIMIT=${#STRING}
+      while (( $i < $LIMIT ))
+      do
+         s=${STRING:$i:1}
+         NEWDIR="$NEWDIR/$s*"
+         (( i += 1 ))
+      done
+      
+   else
+      # concat fragments and append start to them 
+      for d in $@ ; do
+        if [[ $NEWDIR == '' ]]; then 
+            NEWDIR=/$d
+        else     
+            NEWDIR="$NEWDIR*/$d"
+        fi    
+      done
+      NEWDIR="$NEWDIR*" # add last asterisk
+   fi 
+   echo cd $NEWDIR
+   builtin cd $NEWDIR
+     
 }
